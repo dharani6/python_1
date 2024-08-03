adba1f18b11af391ac26c0b42cfd58d268e24efd
dharani6 <32707382+dharani6@users.noreply.github.com>
2024-08-03 23:30:25 +0530

    Update Day1.py

--- Python_new_challenge/Day1.py
+++ Python_new_challenge/Day1.py
@@ -1,2 +1,3 @@
 #Print Function
-print("dhaani", "This August month", True, False, 0308, 2984, sep =" ## ", end='/n')
+print("dhaani", "This August month", True, False, 0308, 2984, sep =" ## ", end='/n') # note down ERROR 
+print("dhaani", "This August month", True, False, 308, 2984, sep =" ## ", end='/n') # note down

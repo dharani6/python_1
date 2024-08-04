0e5c619ee0d621ffea5edf373d7088ff72525105
dharani6 <32707382+dharani6@users.noreply.github.com>
2024-08-04 11:47:57 +0530

    Update Table.py
    Updated the code with function.

--- Python_new_challenge/Table.py
+++ Python_new_challenge/Table.py
@@ -2,3 +2,11 @@
 n = int(input("enter the table Num:"))
 for i in range(1,20):
     print(str(n),'x', str(i),'=', n*i)
+
+
+def Table(num):
+    for i in range(1,21):
+        return (str(n),'x', str(i),'=', n*i)
+
+
+Table(int(input("enter the Table Number:")))

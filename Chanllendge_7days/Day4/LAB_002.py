9aa29486bd23c7ac668be3200c93804440fa9457
Dharanitharan <32707382+dharani6@users.noreply.github.com>
2024-08-12 11:46:21 +0530

    Challenge-Day-4

---
+++ Chanllendge_7days/Day4/LAB_002.py
@@ -0,0 +1,19 @@
+num = 8
+if num == 5:
+    print(str(num), "is not 5")
+else:
+    print(str(num),"Num is greter that 5",sep=" -- ")
+num1 = int(input("Enter the Nummber 1"))
+num2 = int(input("Enter the Nummber 2"))
+num3 = int(input("Enter the Nummber 3"))
+num4 = int(input("Enter the Nummber 4"))
+
+
+if num1 > num2 and num1 > num3 and num1 > num4:
+    print(f"num1--{num1} is greter of all the given number/n")
+elif num2 > num1 and num2 > num3 and num2 > num4:
+        print(f"num2--{num2} is greter of all the given number/n")
+elif num3 > num1 and num3 > num2 and num3 > num4:
+        print(f"num3--{num3} is greter of all the given number/n")
+elif num4 > num1 and num4 > num2 and num4 > num3:
+        print(f"number- {num4} is greter of all the given number/n")
---
+++ Chanllendge_7days/Day4/Lab_001.py
@@ -0,0 +1,9 @@
+# condition and loops
+
+age = int(input("enter your AGE!"))
+
+if age > 18:
+    print("Your eligible for watching the movie(Adult)")
+elif age < 28:
+    print("Your not eligible for watching the movie")
+

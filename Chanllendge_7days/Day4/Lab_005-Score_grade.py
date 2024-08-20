0adede2439cbd2a31199f5c1ac905e73e3ca4bd8
Dharanitharan <32707382+dharani6@users.noreply.github.com>
2024-08-20 15:09:45 +0530

    Check in new fiels with task on 13/08

--- Chanllendge_7days/Day4/Lab_005-Score_grade.py
+++ Chanllendge_7days/Day4/Lab_005-Score_grade.py
@@ -15,11 +15,11 @@ Score = int(input("Enter the Score you got!!!\n"))
 
 
 def Grade_system(Score):
-    if Score < 100 and Score >= 90:
+    if 100 > Score >= 90:# 90>= Score <=100
         print("your grade is A")
-    elif Score <= 89 and Score >= 80:
+    elif 89 >= Score >= 80:
         print("your grade is B")
-    elif Score <= 79 and Score >= 70:
+    elif 79 >= Score >= 70:
         print("your grade is C")
     elif Score <= 69 and Score >= 60:
         print("your grade is D")
@@ -31,8 +31,8 @@ def Grade_system(Score):
 
 Grade_system(Score)
 
-liis = [30, 40, 50, 60]
+#liis = [30, 40, 50, 60]
 '''print(list(map,Grade_system(liis)))'''
 
-for i in liis:
-    Grade_system(i)
+#for i in liis:
+ #   Grade_system(i)
